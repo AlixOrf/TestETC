19d74035e428d772d8cb00e46705ac35b2b20f67
AlixOrf <alix.orfeuvre@epitech.digital>
2024-06-06 16:41:47 +0200

    Mouvement

--- Test/Assets/Script/Mouvement.cs
+++ Test/Assets/Script/Mouvement.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
+    public GameObject world;
     public float horizontalInput;
     public float verticalInput;
+    public enum orientation { Left, Right, Up, Down };
+    public orientation myorientation;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +22,51 @@ public class Mouvement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput);
-        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput,Space.World);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput, Space.World);
+
+        Tourne();
+    }
+
+    void Tourne()
+    {
+        if(horizontalInput > 0)
+        {
+            myorientation = orientation.Right;
+        }
+        else if (horizontalInput < 0)
+        {
+            myorientation = orientation.Left;
+        }
+        if(verticalInput > 0)
+        {
+            myorientation = orientation.Up;
+        }
+        else if (verticalInput < 0)
+        {
+            myorientation = orientation.Down;
+        }
+
+        if (myorientation == orientation.Right) {
+            Quaternion right = Quaternion.Euler(0, 90, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, right, 0.1f );
+        }
+        else if (myorientation == orientation.Left)
+        {
+            Quaternion left = Quaternion.Euler(0, -90, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, left, 0.1f);
+        }
+        else if (myorientation == orientation.Up)
+        {
+            Quaternion up = Quaternion.Euler(0, 0, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, up, 0.1f);
+        }
+        else if (myorientation == orientation.Down)
+        {
+            Quaternion down = Quaternion.Euler(0, 180, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, down, 0.1f);
+        }
     }
 }
+
+
